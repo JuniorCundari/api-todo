578ddf8b40f43acdcf27eb0f6ef69db98253aa70
Junior Cundari <jrcundari@yahoo.com.br>
2023-06-19 11:13:49 -0300

    feat: environment variables

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,11 +1,13 @@
 import { Client } from 'pg'
 
+const port = process.env.DB_PORT as number | undefined
+
 const client = new Client({
-  host: 'localhost',
-  port: 5432,
-  user: 'root',
-  password: 'root',
-  database: 'todos',
+  host: process.env.DB_HOST,
+  port,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 })
 
 client.connect()
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
 import express from 'express'
+import 'express-async-errors'
 import 'dotenv/config'
 
 import routes from './routes'
+import errorHandler from './app/middlewares/errorHandler'
+import cors from './app/middlewares/cors'
 
 const PORT = process.env.PORT || 4000
 
 const app = express()
 
 app.use(express.json())
+app.use(cors)
 app.use(routes)
+app.use(errorHandler)
 
-app.listen(3000, () =>
+app.listen(`${PORT}`, () =>
   console.log(`🔥 Server started at http://localhost:${PORT}`),
 )

119016c1764db3eead21dfa0fe5414ee73a929e3
Junior Cundari <jrcundari@yahoo.com.br>
2023-07-05 21:18:26 -0300

    feat: new environment variable and set header cors

--- src/app/middlewares/cors.ts
+++ src/app/middlewares/cors.ts
@@ -1,11 +1,30 @@
 import { Request, Response, NextFunction } from 'express'
 
-const PORT = process.env.PORT
+const PORT_ORIGIN = process.env.PORT_ORIGIN
 
 export default (request: Request, response: Response, next: NextFunction) => {
-  response.setHeader('Access-Control-Allow-Origin', `http://localhost:${PORT}`)
+  response.setHeader(
+    'Access-Control-Allow-Origin',
+    `http://localhost:${PORT_ORIGIN}`,
+  )
   response.setHeader('Access-Control-Allow-Methods', '*')
-  response.setHeader('Access-Control-Allow-Headers', '*')
+  response.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Api-Key, X-Requested-With, Content-Type, Accept, Authorization',
+  )
   response.setHeader('Access-Control-Max-Age', '7200')
-  next()
+  response.setHeader('Access-Control-Allow-Credentials', 'true')
+
+  if (request.method === 'OPTIONS') {
+    response.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+    response.setHeader(
+      'Access-Control-Allow-Headers',
+      'Content-Type, Authorization',
+    )
+    response.setHeader('Access-Control-Max-Age', '86400')
+
+    response.sendStatus(204)
+  } else {
+    next()
+  }
 }
